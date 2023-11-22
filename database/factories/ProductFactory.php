a7c43ef5dd1cb6e75c98ffa2c2dda23bd364257b
kamrantekkit <mdkam02ali@gmail.com>
2023-11-22 22:03:19 +0000

    feat(product): added product factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->text(),
+            'archived' => $this->faker->boolean(),
+            'image_path' => $this->faker->word(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ];
+    }
+}
