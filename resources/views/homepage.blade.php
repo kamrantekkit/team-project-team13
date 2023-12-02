f369bd1762d16e0b2c91040427daf2f7efa24441
ray9905 <220105514@aston.ac.uk>
2023-12-02 05:18:48 +0000

    core: created playstation, xbox and nintendo buttons
    added images for each of the buttons and fixed spacing issues

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,49 +1,70 @@
 <!DOCTYPE html>
 <html data-bs-theme="dark" lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>TP version 1</title>
-    <!-- Bootstrap css link -->
-    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
-
+    <title>homepage version 2</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
 </head>
+
 <body style="background: rgb(255,183,253);">
+
+<!-- Navigation bar -->
 <nav class="navbar navbar-expand-md navbar-fixed-top navigation-clean-button navbar-light" style="background: rgb(34, 34, 34); padding-top: 0; padding-bottom: 10px;">
     <div class="container">
-        <!-- Navbar toggler button for screen size -->
+        <!-- Navbar toggler button for smaller screen sizes -->
         <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-1">
             <span class="visually-hidden">Toggle navigation</span>
             <span class="navbar-toggler-icon"></span>
         </button>
         <div>
-            <!-- Playportal navbar logo -->
-            <a class="navbar-brand" href="" style="color: white; font-size: 24px; font-family:  sans-serif;"><span>PlayPortal</span></a>
+            <!-- Navbar brand ( -->
+            <a class="navbar-brand" href="#" style="color: white; font-size: 24px; font-family: sans-serif;"><span>PlayPortal</span></a>
         </div>
         <div id="navcol-1" class="collapse navbar-collapse" style="color: rgb(255, 255, 255);">
-            <!-- Navbar links -->
+            <!-- Navigation Links -->
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item"><a class="nav-link active" href="home.html" style="color: lightgrey; font-size: 18px;">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="games.html" style="color: lightgrey; font-size: 18px;">Games</a></li>
                 <li class="nav-item"><a class="nav-link" href="consoles.html" style="color: lightgrey; font-size: 18px;">Consoles</a></li>
-                <li class="nav-item"><a class="nav-link" href="accessories.html" style="color: lightgrey; font-size: 18px;">Accessories</a></li>
+                <li class="nav-item"><a class="nav-link" href="faq accessories.html" style="color: lightgrey; font-size: 18px;">Accessories</a></li>
                 <li class="nav-item"><a class="nav-link" href="about.html" style="color: lightgrey; font-size: 18px;">About us</a></li>
             </ul>
+            <!-- Log in and sign up buttons along with their respective links-->
             <p class="ms-auto navbar-text actions" style="text-align: right;">
-                <!-- Log in link -->
                 <a class="login" href="login.html" style="color: lightgrey; font-size: 18px; margin-right: 15px; text-decoration: none;">Log In</a>
-                <!-- Sign up link and sign up button -->
                 <a class="btn btn-primary action-button" role="button" href="signup.html" style="color: black; background: white; border-radius: 10px; font-size: 18px; padding: 10px 20px; border: none;">Sign Up</a>
             </p>
         </div>
     </div>
 </nav>
-<div style="height: 500px;background: rgb(34,34,34);">
-    <div style="background: #ffffff;height: 411.6667px;margin-bottom: 100px;margin-top: 400px;"></div>
+
+<!-- Content sections -->
+<div style="height: 500px; background: rgb(34,34,34);">
+    <div style="background: #ffffff; height: 411.6667px; margin-bottom: 100px; margin-top: 400px;">
+        <div class="container" style="min-height: 400px;">
+            <div class="row justify-content-center">
+                <!-- Nintendo button and image -->
+                <div class="col-md-4 text-center" style="margin-top: 30px;">
+                    <button class="btn mb-4" style="width: 70%; background-color: #D90011; color: #ffffff;">Nintendo</button>
+                    <img class="img-fluid" src="assets/img/Screenshot%202023-11-22%20013112.png" alt="Nintendo img" />
+                </div>
+                <!-- Xbox button and image -->
+                <div class="col-md-4 text-center" style="margin-top: 30px;">
+                    <button class="btn mb-4" style="width: 70%; background-color: #127D10; color: #ffffff;">Xbox</button>
+                    <img class="img-fluid" src="assets/img/Screenshot%202023-11-22%20025302.png" alt="Xbox img" />
+                </div>
+                <!-- Playstation button and image-->
+                <div class="col-md-4 text-center" style="margin-top: 30px;">
+                    <button class="btn mb-4" style="width: 70%; background-color: #024EA2; color: #ffffff;">Playstation</button>
+                    <img class="img-fluid" src="assets/img/Screenshot%202023-11-22%20025327.png" alt="Playstation img" />
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <section></section>
-<!-- Links the bootstrap JS -->
-<script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
-
