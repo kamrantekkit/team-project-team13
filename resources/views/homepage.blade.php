e443c21870800249c90e952d5a71c2f9628f426e
ray9905 <220105514@aston.ac.uk>
2023-12-03 00:07:51 +0000

    core: created remaining PC and Accessories buttons along with their images

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,67 +4,91 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>homepage version 2</title>
+    <title>homepage version 3</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+
 </head>
 
-<body style="background: rgb(255,183,253);">
-
-<!-- Navigation bar -->
-<nav class="navbar navbar-expand-md navbar-fixed-top navigation-clean-button navbar-light" style="background: rgb(34, 34, 34); padding-top: 0; padding-bottom: 10px;">
-    <div class="container">
-        <!-- Navbar toggler button for smaller screen sizes -->
-        <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-1">
-            <span class="visually-hidden">Toggle navigation</span>
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div>
-            <!-- Navbar brand ( -->
-            <a class="navbar-brand" href="#" style="color: white; font-size: 24px; font-family: sans-serif;"><span>PlayPortal</span></a>
-        </div>
-        <div id="navcol-1" class="collapse navbar-collapse" style="color: rgb(255, 255, 255);">
-            <!-- Navigation Links -->
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link active" href="home.html" style="color: lightgrey; font-size: 18px;">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="games.html" style="color: lightgrey; font-size: 18px;">Games</a></li>
-                <li class="nav-item"><a class="nav-link" href="consoles.html" style="color: lightgrey; font-size: 18px;">Consoles</a></li>
-                <li class="nav-item"><a class="nav-link" href="faq accessories.html" style="color: lightgrey; font-size: 18px;">Accessories</a></li>
-                <li class="nav-item"><a class="nav-link" href="about.html" style="color: lightgrey; font-size: 18px;">About us</a></li>
-            </ul>
-            <!-- Log in and sign up buttons along with their respective links-->
-            <p class="ms-auto navbar-text actions" style="text-align: right;">
-                <a class="login" href="login.html" style="color: lightgrey; font-size: 18px; margin-right: 15px; text-decoration: none;">Log In</a>
-                <a class="btn btn-primary action-button" role="button" href="signup.html" style="color: black; background: white; border-radius: 10px; font-size: 18px; padding: 10px 20px; border: none;">Sign Up</a>
-            </p>
-        </div>
-    </div>
-</nav>
-
-<!-- Content sections -->
-<div style="height: 500px; background: rgb(34,34,34);">
-    <div style="background: #ffffff; height: 411.6667px; margin-bottom: 100px; margin-top: 400px;">
-        <div class="container" style="min-height: 400px;">
-            <div class="row justify-content-center">
-                <!-- Nintendo button and image -->
-                <div class="col-md-4 text-center" style="margin-top: 30px;">
-                    <button class="btn mb-4" style="width: 70%; background-color: #D90011; color: #ffffff;">Nintendo</button>
-                    <img class="img-fluid" src="assets/img/Screenshot%202023-11-22%20013112.png" alt="Nintendo img" />
-                </div>
-                <!-- Xbox button and image -->
-                <div class="col-md-4 text-center" style="margin-top: 30px;">
-                    <button class="btn mb-4" style="width: 70%; background-color: #127D10; color: #ffffff;">Xbox</button>
-                    <img class="img-fluid" src="assets/img/Screenshot%202023-11-22%20025302.png" alt="Xbox img" />
-                </div>
-                <!-- Playstation button and image-->
-                <div class="col-md-4 text-center" style="margin-top: 30px;">
-                    <button class="btn mb-4" style="width: 70%; background-color: #024EA2; color: #ffffff;">Playstation</button>
-                    <img class="img-fluid" src="assets/img/Screenshot%202023-11-22%20025327.png" alt="Playstation img" />
-                </div>
-            </div>
-        </div>
-    </div>
+<body style="background: rgb(255,183,253);height: 1500px;"><nav class="navbar navbar-expand-md navbar-fixed-top navigation-clean-button navbar-light" style="background: rgb(34, 34, 34); padding-top: 0; padding-bottom: 10px;">
+<div class="container">
+    <!-- Navbar toggler button for smaller screen sizes -->
+    <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-1">
+<span class="visually-hidden">Toggle navigation</span>
+<span class="navbar-toggler-icon"></span>
+</button>
+<div>
+    <!-- Navbar branding -->
+    <a class="navbar-brand" href="#" style="color: white; font-size: 24px; font-family: sans-serif;"><span>PlayPortal</span></a>
 </div>
-<section></section>
-<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<div id="navcol-1" class="collapse navbar-collapse" style="color: rgb(255, 255, 255);">
+    <!-- Navigation links -->
+    <ul class="navbar-nav ms-auto">
+<li class="nav-item"><a class="nav-link active" href="index.html" style="color: lightgrey; font-size: 18px;">Home</a></li>
+<li class="nav-item"><a class="nav-link" href="about.html" style="color: lightgrey; font-size: 18px;">Games</a></li>
+<li class="nav-item"><a class="nav-link" href="#" style="color: lightgrey; font-size: 18px;">Consoles</a></li>
+<li class="nav-item"><a class="nav-link" href="faq.html" style="color: lightgrey; font-size: 18px;">Accessories</a></li>
+<li class="nav-item"><a class="nav-link" href="contact.html" style="color: lightgrey; font-size: 18px;">About us</a></li>
+</ul>
+    <!-- Log in and sign up buttons along with their respective links -->
+<p class="ms-auto navbar-text actions" style="text-align: right;">
+<a class="login" href="login.html" style="color: lightgrey; font-size: 18px; margin-right: 15px; text-decoration: none;">Log In</a>
+<a class="btn btn-primary action-button" role="button" href="signup.html" style="color: black; background: white; border-radius: 10px; font-size: 18px; padding: 10px 20px; border: none; transition: background 0.3s;">Sign Up</a>
+</p>
+</div>
+</div>
+</nav>
+<!-- Content Section -->
+    <div style="height: 0px;background: rgb(34,34,34);">
+        <div style="background: #ffffff;margin-bottom: 100px;margin-top: 400px;color: rgb(255,183,253);"><div class="container" style="min-height: 400px;">
+  <div class="row justify-content-center">
+    <!-- Nintendo button and images-->
+    <div class="col-md-4 text-center" style="margin-top: 30px;">
+      <button class="btn mb-4" style="width: 70%; background-color: #D90011; color: #ffffff;">Nintendo</button>
+      <img class="img-fluid" src="assets/img/Screenshot%202023-11-22%20013112.png" alt="Nintendo img" />
+    </div>
+    <!-- Xbox button and images -->
+    <div class="col-md-4 text-center" style="margin-top: 30px;">
+      <button class="btn mb-4" style="width: 70%; background-color: #127D10; color: #ffffff;">Xbox</button>
+      <img class="img-fluid" src="assets/img/Screenshot%202023-11-22%20025302.png" alt="Xbox img" />
+    </div>
+    <!-- Playstation button and images -->
+    <div class="col-md-4 text-center" style="margin-top: 30px;">
+      <button class="btn mb-4" style="width: 70%; background-color: #024EA2; color: #ffffff;">Playstation</button>
+      <img class="img-fluid" src="assets/img/Screenshot%202023-11-22%20025327.png" alt="Playstation img" />
+    </div>
+    <!-- PC button and images -->
+    <div class="col-md-4 text-center" style="margin-top: 30px;">
+      <button class="btn mb-4" style="width: 70%; background-color: #225B7C; color: #ffffff;">PC</button>
+      <img class="img-fluid" src="assets/img/Screenshot%202023-11-22%20031453.png" style="width: 300px;" alt="PC img" />
+    </div>
+  </div>
+</div>
+            <!-- Accessories button and 3 images -->
+            <div></div><div class="container-fluid text-center" style="height: 250px; background: var(--bs-secondary-border-subtle); margin-top: 50px;">
+  <div class="row justify-content-evenly align-items-center h-100">
+    <div class="col-md-3 mt-4">
+      <a href="#" class="btn btn-primary w-100 mb-3" style="margin-top: -170px; background-color: #000; border: none;">Accessories</a>
+    </div>
+  </div>
+  <div class="row justify-content-evenly align-items-center h-100">
+    <div class="col-md-3 mb-3" style="margin-top: -420px;">
+      <img src="assets/img/Screenshot%202023-11-27%20231603.png" style="width: 175px;" />
+    </div>
+    <div class="col-md-3 mb-3" style="margin-top: -420px;">
+      <img src="assets/img/Screenshot%202023-11-27%20231542.png" style="width: 175px;" />
+    </div>
+    <div class="col-md-3 mb-3" style="margin-top: -420px;">
+      <img src="assets/img/Screenshot%202023-11-27%20214411.png" style="width: 175px;" />
+    </div>
+  </div>
+</div>
+
+            <div style="background: rgb(34, 34, 34);height: 68.3333px;margin-top: 230px;"></div>
+        </div>
+    </div>
+    <section></section>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/Navbar---Apple-navbar---apple.js"></script>
 </body>
+
 </html>
