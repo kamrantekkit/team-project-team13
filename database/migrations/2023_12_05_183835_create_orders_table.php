585ca1e16af6d11fb977530e8bc1ca05e57c1385
kamrantekkit <mdkam02ali@gmail.com>
2023-12-10 10:54:47 +0000

    fix: orders table, mismatched value type in foreign key relationship

--- database/migrations/2023_12_05_183835_create_orders_table.php
+++ database/migrations/2023_12_05_183835_create_orders_table.php
@@ -10,7 +10,7 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
             $table->string('email');
             $table->integer('price');
             $table->string('payment_id');
