09bb48d8f43d88fedd8ac96d77b79b315e25f918
kamrantekkit <mdkam02ali@gmail.com>
2023-12-11 19:07:44 +0000

    chore: added config for stackhero db

--- config/database.php
+++ config/database.php
@@ -43,6 +43,25 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'maria' => [
+            'driver' => 'mysql',
+            'host' => env('DATABASE_URL'),
+            'port' => 3306,
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'database' => env('DB_DATABASE'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'sslmode' => 'require',
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
