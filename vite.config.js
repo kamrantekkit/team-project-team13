500d11325d07d911c10ce07f81df7281c4616120
kamrantekkit <mdkam02ali@gmail.com>
2023-12-12 13:22:29 +0000

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/sass/Login.css',
+                'resources/sass/Style.css',
                 'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
