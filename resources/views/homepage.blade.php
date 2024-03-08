2c17301b4e426451b8cce67c9bd6cd85717648c2
kamrantekkit <mdkam02ali@gmail.com>
2024-03-08 22:16:46 +0000

    core(home page): integrated new home page and restructured nav bar layout

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -11,7 +11,8 @@
 
 </head>
 <!--  page background -->
-<body style="background: rgb(250,250,245);height: 1500px;"><nav class="navbar navbar-expand-md navbar-fixed-top navigation-clean-button navbar-light" style="background: rgb(34, 34, 34);padding-top: 0;padding-bottom: 10px;height: 89px;">
+<body style="background: rgb(250,250,245);height: 1500px;">
+<nav class="navbar navbar-expand-md navbar-fixed-top navigation-clean-button navbar-light" style="background: rgb(34, 34, 34);padding-top: 0;padding-bottom: 10px;height: 89px;">
     <!--  Navbar section -->
     <div class="container"><button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button><a class="navbar-brand" href="#" style="color: white;font-size: 24px;font-family: 'Roboto', sans-serif;">PlayPortal</a>
         <div id="navcol-1" class="collapse navbar-collapse">
@@ -39,9 +40,9 @@
             </ul>
         </div>
     </div>
-</nav><div></div><img id="imageSwitch" src="assets/img/Funko-Pokemon-db.webp" style="height: auto; width: 100vw; max-width: 100%; position: relative; display: block; margin: 0 auto;">
+</nav>
 
-
+<div><img id="imageSwitch" src="assets/img/Funko-Pokemon-db.webp" style="height: auto; width: 100vw; max-width: 100%; position: relative; display: block; margin: 0 auto;"></div>
 
 <!-- Content Section -->
 <div style="height: 0px;background: rgb(34,34,34);">
--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,17 +1,13 @@
 
-<nav class="navbar navbar-expand-md navbar-fixed-top navigation-clean-button navbar-light" style="background: rgb(34, 34, 34); padding-top: 0; padding-bottom: 10px; height: 89px;">
-    <div class="container">
-        <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-1">
-            <span class="visually-hidden">Toggle navigation</span>
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand" href="{{route('home')}}" style="color: white; font-size: 24px; font-family: 'Roboto', sans-serif;">PlayPortal</a>
+<nav class="navbar navbar-expand-md navbar-fixed-top navigation-clean-button navbar-light" style="background: rgb(34, 34, 34);padding-top: 0;padding-bottom: 10px;height: 89px;">
+    <!--  Navbar section -->
+    <div class="container"><button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <a class="navbar-brand" href="{{route('home')}}"  style="color: white;font-size: 24px;font-family: 'Roboto', sans-serif;">PlayPortal</a>
         <div id="navcol-1" class="collapse navbar-collapse">
             <ul class="navbar-nav me-auto">
-                <li class="nav-item"><a class="nav-link active" href="{{route('home')}}" style="color: lightgrey; font-size: 18px;">Home</a></li>
-{{--                <li class="nav-item"><a class="nav-link" href="about.html" style="color: lightgrey; font-size: 18px;">Games</a></li>--}}
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" style="color: lightgrey; font-size: 18px;">Consoles</a>
+                <li class="nav-item"><a class="nav-link active" href="{{route('home')}}" style="color: lightgrey;font-size: 18px;">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="about.html" style="color: lightgrey;font-size: 18px;">Games</a></li>
+                <li class="nav-item dropdown"><a id="navbarDropdown" class="nav-link dropdown-toggle"  data-bs-toggle="dropdown" href="#" role="button" style="color: lightgrey;font-size: 18px;"> Consoles </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color: rgb(34, 34, 34);">
                         <li><a class="dropdown-item" href="{{route('products',['Playstation'])}}" style="color: lightgrey;">PlayStation</a></li>
                         <li><a class="dropdown-item" href="{{route('products',['Nintendo'])}}" style="color: lightgrey;">Nintendo</a></li>
@@ -19,24 +15,22 @@
                         <li><a class="dropdown-item" href="{{route('products',['PC'])}}" style="color: lightgrey;">PC</a></li>
                     </ul>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="{{route('products',['Accessories'])}}" style="color: lightgrey; font-size: 18px;">Accessories</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{route('about-us')}}" style="color: lightgrey; font-size: 18px;">About us</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{route('products',['Accessories'])}}" style="color: lightgrey;font-size: 18px;">Accessories</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.html" style="color: lightgrey;font-size: 18px;">Contact us</a></li>
             </ul>
             <form class="d-flex navbar-form">
-                <div class="input-group">
-                    <input class="form-control form-control" type="search" placeholder="Search" />
-                    <button class="btn btn-outline-light" type="submit">Search</button>
-                </div>
+                <div class="input-group"><input class="form-control form-control" type="search" placeholder="Search" /><button class="btn btn-outline-light" type="submit">Search</button></div>
             </form>
             <ul class="navbar-nav">
                 @if(auth()->guest())
-                <li class="nav-item"><a class="nav-link" href="{{route('login')}}" style="color: lightgrey; font-size: 18px;">Log In</a></li>
-                <li class="nav-item"><a class="btn btn-primary action-button" role="button" href="{{route('register')}}" style="color: black; background: white; border-radius: 10px; font-size: 18px; padding: 10px 20px; border: none; transition: background 0.3s;">Sign Up</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{route('login')}}" style="color: lightgrey;font-size: 18px;">Log In</a></li>
+                <li class="nav-item"><a class="btn btn-primary action-button" role="button" href="{{route('register')}}" style="color: black;background: white;border-radius: 10px;font-size: 18px;padding: 10px 20px;border: none;transition: background 0.3s;">Sign Up</a></li>
                 @endif
                 @if(auth()->check())
-                        <li class="nav-item"><a class="btn btn-primary action-button" role="button" href="{{route('dashboard')}}" style="color: black; background: white; border-radius: 10px; font-size: 18px; padding: 10px 20px; border: none; transition: background 0.3s;">Dashboard</a></li>
+                        <li class="nav-item"><a class="btn btn-primary action-button" role="button" href="{{route('dashboard')}}" style="color: black;background: white;border-radius: 10px;font-size: 18px;padding: 10px 20px;border: none;transition: background 0.3s;">Dashboard</a></li>
                 @endif
-                <li class="nav-item"><a class="btn btn-outline-light ms-2" href="{{route('basket')}}" style="margin-top: 5px;" type="button"><i class="fas fa-shopping-basket" style="color: white;"></i> Basket</a></li>
+
+                <li class="nav-item"><button class="btn btn-outline-light ms-2" style="margin-top: 5px;" type="button"><i class="fas fa-shopping-basket"  style="color: white;"></i> Basket </button></li>
             </ul>
         </div>
     </div>
