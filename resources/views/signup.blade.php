5e9739218e4e16e4dba065790c1459f20feac8dd
khalifaalmah <220036609@aston.ac.uk>
2024-03-13 17:21:25 +0000

    enhance the signup page design

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 <!DOCTYPE html>
 <html data-bs-theme="light" lang="en">
 
@@ -11,16 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Signup</title>
-    {{-- <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400|Roboto:300,400,700">
-    <link rel="stylesheet" href="assets/css/Black-Navbar.css">
-    <link rel="stylesheet" href="assets/css/Navbar---Apple-navbar---apple.css">
-    <link rel="stylesheet" href="assets/css/Navbar---Apple.css">
-    <link rel="stylesheet" href="assets/css/Navbar-Right-Links-Dark-icons.css"> --}}
     @vite(['resources/sass/app.scss'])
 </head>
 
-<body style="background: rgb(255,183,253);">
+<body style="">
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <nav class="navbar navbar-expand-md navbar-fixed-top navigation-clean-button navbar-light" style="background: rgb(34, 34, 34);border-radius: 20;border-top-left-radius: 20;border-top-right-radius: 20;border-bottom-right-radius: 20;border-bottom-left-radius: 20;padding-top: 0;padding-bottom: 10px;">
         <div class="container"><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
@@ -37,25 +25,21 @@
             </div>
         </div>
     </nav>
-    <div style="background: rgb(255,183,248);height: 500px;border-color: var(--bs-body-bg);">
+    <div style="height: 500px;border-color: var(--bs-body-bg);">
         <div class="container py-4 py-xl-5">
             <div class="row">
                 <div class="col-md-6 col-sm-12 offset-md-3">
-                    <div class="card">
+                    <div class="card bg-white shadow-lg" style="border: none">
                         <div class="card-body">
 
-                                <h5 class="card-title" style="text-align: center">Signup</h5>
+                                <h5 class="card-title" style="text-align: center;    font-weight: bolder;">Signup</h5>
                                 <input type="email" placeholder="E-mail" class="form-control mt-3"/>
                                 <input type="password" placeholder="Password" class="form-control mt-3"/>
                                 <input type="password" placeholder="Confirm password" class="form-control mt-3"/>
                                 <div class="mt-3" style="display: flex;justify-content: space-around;flex-wrap: wrap; ">
                                     {{-- <button class="btn btn-secondary mt-1">I'am not  a robot</button> --}}
-                                    <button class="btn btn-primary mt-1">Create Account</button>
+                                    <button class="btn btn-dark mt-1">Create Account</button>
                                 </div>
-
-
-
-
                         </div>
                     </div>
                 </div>
@@ -63,9 +47,7 @@
 
         </div>
     </div>
-    <div style="height: 500px;background: rgb(34,34,34);">
-        <div style="background: #ffffff;height: 411.6667px;margin-bottom: 100px;margin-top: 400px;"></div>
-    </div>
+
     <section>
     </section>
     {{-- <script src="assets/bootstrap/js/bootstrap.min.js"></script>
