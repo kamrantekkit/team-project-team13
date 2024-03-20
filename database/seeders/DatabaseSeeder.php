b2613b876c1537a2b08e928f3e6237b1cab966af
kamrantekkit <mdkam02ali@gmail.com>
2024-03-20 01:19:32 +0000

    core(database seeder): centralised seeding for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(UserSeeder::class);
+        $this->call(ProductSeeder::class);
+        $this->call(TagSeeder::class);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
