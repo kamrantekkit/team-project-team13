fec34055fd8e5f8d9601847d72530a85549762d5
ray9905 <220105514@aston.ac.uk>
2024-03-27 01:25:39 +0000

    core: fixed design of the page

--- resources/views/customer_management.blade.php
+++ resources/views/customer_management.blade.php
@@ -85,3 +85,49 @@
 </body>
 
 </html>
+@extends('layout.admin_layout_dashboard')
+
+
+
+@section('content')
+    <h1 class="playportal-page-title">Customer Management</h1>
+    <form method="GET" class="d-flex navbar-form ps-5 pe-5 pb-2 pt-2"
+          action="{{route("admin.customers-management.search")}}">
+        {{@csrf_field()}}
+        <div class="input-group">
+            <input class="form-control form-control border-black" name="query" type="search"
+                   placeholder="Search"/>
+            <button class="btn btn-outline-dark" type="submit">Search</button>
+        </div>
+    </form>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Email</th>
+            <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($customers as $customer)
+            <tr>
+                <th scope="row">{{$customer['id']}}</th>
+                <td>{{$customer['name']}}</td>
+                <td>{{$customer['phone']}}</td>
+                <td>{{$customer['email']}}</td>
+                <td>
+                    <button type="button" class="btn btn-primary btn-sm">
+                        Edit <i class="fas fa-edit"></i>
+                    </button>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    <div class="row">
+        {{ $customers->links('pagination::bootstrap-5') }}
+    </div>
+
+@endsection
